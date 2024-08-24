0b326f8885433e6b08edac239c4494e824d5db45
Daniel Brown <daniel.m.brown27@gmail.com>
2024-08-24 16:48:53 +1000

    Missed src folder

---
+++ src/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+    // jQuery methods go here...
+    console.log("Ready!");
+
+    $("#blarp-button").click(function(){
+        console.log("Button clicked");
+        $("#blarp-button").hide();
+    })
+  });
