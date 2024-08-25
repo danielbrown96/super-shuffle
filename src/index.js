d42ff5928630dee68b303b6542bd50783353c253
Daniel Brown <daniel.m.brown27@gmail.com>
2024-08-25 17:05:23 +1000

    Working on webpack config and loading data

--- src/index.js
+++ src/index.js
@@ -1,34 +1,14 @@
 import { test } from './functions/test.js';
+import heroes from '../data/heroes.json';
 
 $(document).ready(function(){
 
     // jQuery methods go here...
     console.log("Ready!");
 
-    $("#load-button").click(function(){
-        //test();
-        readFiles();
+    console.log(heroes);
+
+    $("#test-button").click(function(){
+        test();
     })
 });
-
-
-var readFiles = async () => {
-    var data = {
-        heroes: []
-    }
-
-    await fetch('https://api.github.com/repos/danielbrown96/super-shuffle/contents/data/heroes').then(response => response.json())
-    .then(async json => {
-        await Promise.all(
-        json.map(e => {
-            if (e.name != 'template.json'){
-                var fullPath = `https://danielbrown96.github.io/super-shuffle/${e.path}`;
-                return fetch(fullPath).then(response => response.json()).then(json => {
-                    data.heroes.push(json);
-                })
-            }
-        }))
-    })
-
-    console.log(data);
-}
